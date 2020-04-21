4ca0ab629976632ac8cff37f7e673c539b1dec42
Edivaldo Silva <piqueno43@gmail.com>
2020-04-21 08:37:55 -0300

    Preparando variavel ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.11:3333',
+  baseURL: process.env.API_URL,
 });
 
 export default api;
