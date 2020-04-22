830caf2556ac532037d19b81c81da82394f151b4
Edivaldo Silva <piqueno43@gmail.com>
2020-04-22 11:22:13 -0300

    Add .env.production file

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
 });
 
 export default api;
